e54bedaf4beab5b0d02ebdbaa53ba6d81ee188dd
String <string.gle@gmail.com>
2020-07-11 01:53:38 +0900

    no message

--- gao4yu83/hint2/script.js
+++ gao4yu83/hint2/script.js
@@ -1,6 +1,6 @@
 var initNum = () => {
     return {
-        w1r1c1: 1,
+        w1r1c1: -1,
         w1r1c2: 4,
         w1r2c1: 6,
         w1r2c2: 3,
@@ -31,7 +31,8 @@ var app = new Vue({
     el: "#app",
     data: {
         n: initNum(),
-        c: false
+        c: false,
+        m: ""
     },
     methods: {
         b1: function() {
@@ -119,65 +120,65 @@ var app = new Vue({
         },
         cw1: function() {
             var n = this.n;
-            return n.w1r1c1 === n.w1r1c2 &&
-                n.w1r1c1 === n.w1r2c1 &&
-                n.w1r1c1 === n.w1r2c2;
+            return Math.abs(n.w1r1c1) === Math.abs(n.w1r1c2) &&
+                Math.abs(n.w1r1c1) === Math.abs(n.w1r2c1) &&
+                Math.abs(n.w1r1c1) === Math.abs(n.w1r2c2);
         },
         cw2: function() {
             var n = this.n;
-            return n.w2r1c1 === n.w2r1c2 &&
-                n.w2r1c1 === n.w2r2c1 &&
-                n.w2r1c1 === n.w2r2c2;
+            return Math.abs(n.w2r1c1) === Math.abs(n.w2r1c2) &&
+                Math.abs(n.w2r1c1) === Math.abs(n.w2r2c1) &&
+                Math.abs(n.w2r1c1) === Math.abs(n.w2r2c2);
         },
         cw3: function() {
             var n = this.n;
-            return n.w3r1c1 === n.w3r1c2 &&
-                n.w3r1c1 === n.w3r2c1 &&
-                n.w3r1c1 === n.w3r2c2;
+            return Math.abs(n.w3r1c1) === Math.abs(n.w3r1c2) &&
+                Math.abs(n.w3r1c1) === Math.abs(n.w3r2c1) &&
+                Math.abs(n.w3r1c1) === Math.abs(n.w3r2c2);
         },
         cw4: function() {
             var n = this.n;
-            return n.w4r1c1 === n.w4r1c2 &&
-                n.w4r1c1 === n.w4r2c1 &&
-                n.w4r1c1 === n.w4r2c2;
+            return Math.abs(n.w4r1c1) === Math.abs(n.w4r1c2) &&
+                Math.abs(n.w4r1c1) === Math.abs(n.w4r2c1) &&
+                Math.abs(n.w4r1c1) === Math.abs(n.w4r2c2);
         },
         cw5: function() {
             var n = this.n;
-            return n.w5r1c1 === n.w5r1c2 &&
-                n.w5r1c1 === n.w5r2c1 &&
-                n.w5r1c1 === n.w5r2c2;
+            return Math.abs(n.w5r1c1) === Math.abs(n.w5r1c2) &&
+                Math.abs(n.w5r1c1) === Math.abs(n.w5r2c1) &&
+                Math.abs(n.w5r1c1) === Math.abs(n.w5r2c2);
         },
     },
     computed: {
         num1: function() {
-            return this.n.w1r1c1 + this.n.w1r1c2 + this.n.w1r2c1 + this.n.w1r2c2;
+            return Math.abs(this.n.w1r1c1) + Math.abs(this.n.w1r1c2) + Math.abs(this.n.w1r2c1) + Math.abs(this.n.w1r2c2);
         },
         num2: function() {
-            return this.n.w2r1c1 + this.n.w2r1c2 + this.n.w2r2c1 + this.n.w2r2c2;
+            return Math.abs(this.n.w2r1c1) + Math.abs(this.n.w2r1c2) + Math.abs(this.n.w2r2c1) + Math.abs(this.n.w2r2c2);
         },
         num3: function() {
-            return this.n.w3r1c1 + this.n.w3r1c2 + this.n.w3r2c1 + this.n.w3r2c2;
+            return Math.abs(this.n.w3r1c1) + Math.abs(this.n.w3r1c2) + Math.abs(this.n.w3r2c1) + Math.abs(this.n.w3r2c2);
         },
         num4: function() {
-            return this.n.w4r1c1 + this.n.w4r1c2 + this.n.w4r2c1 + this.n.w4r2c2;
+            return Math.abs(this.n.w4r1c1) + Math.abs(this.n.w4r1c2) + Math.abs(this.n.w4r2c1) + Math.abs(this.n.w4r2c2);
         },
         num5: function() {
-            return this.n.w5r1c1 + this.n.w5r1c2 + this.n.w5r2c1 + this.n.w5r2c2;
+            return Math.abs(this.n.w5r1c1) + Math.abs(this.n.w5r1c2) + Math.abs(this.n.w5r2c1) + Math.abs(this.n.w5r2c2);
         },
         w1c: function() {
-            return this.cw1() ? "c" + this.n.w1r1c1.toString() : "";
+            return this.cw1() ? "c" + Math.abs(this.n.w1r1c1).toString() : "";
         },
         w2c: function() {
-            return this.cw2() ? "c" + this.n.w2r1c1.toString() : "";
+            return this.cw2() ? "c" + Math.abs(this.n.w2r1c1).toString() : "";
         },
         w3c: function() {
-            return this.cw3() ? "c" + this.n.w3r1c1.toString() : "";
+            return this.cw3() ? "c" + Math.abs(this.n.w3r1c1).toString() : "";
         },
         w4c: function() {
-            return this.cw4() ? "c" + this.n.w4r1c1.toString() : "";
+            return this.cw4() ? "c" + Math.abs(this.n.w4r1c1).toString() : "";
         },
         w5c: function() {
-            return this.cw5() ? "c" + this.n.w5r1c1.toString() : "";
+            return this.cw5() ? "c" + Math.abs(this.n.w5r1c1).toString() : "";
         }
     },
     watch: {
@@ -186,6 +187,7 @@ var app = new Vue({
                 if (!this.c && this.cw1() && this.cw2() && this.cw3() && this.cw4() && this.cw5()) {
                     setTimeout(c, 50);
                     this.c = true;
+                    this.m = "password : buckrubies";
                 }
             },
             deep: true
@@ -194,5 +196,5 @@ var app = new Vue({
 });
 
 var c = () => {
-    alert("clear!");
+    alert("clear!\npassword : buckrubies");
 }
